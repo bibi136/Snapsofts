ce6453e72bfa5c91816577750625a07bfd7d2a79
NT Hưng <HungNT@Macbook-Pro.local>
2015-06-29 00:18:09 +0700

    Initial commit

---
+++ app/src/main/java/com/hungnt/login_register/MainActivity.java
@@ -0,0 +1,43 @@
+package com.hungnt.login_register;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername,etPassword;
+    TextView btRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btRegister = (TextView) findViewById(R.id.tvRegister);
+
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etUsername = (EditText) findViewById(R.id.etEmail);
+
+        bLogin = (Button) findViewById(R.id.bLogin);
+
+        btRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvRegister:
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/hungnt/login_register/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.hungnt.login_register;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class RegisterActivity extends ActionBarActivity implements View.OnClickListener{
+
+    Button bRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bRegister:
+                finish();
+        }
+    }
+}
+
