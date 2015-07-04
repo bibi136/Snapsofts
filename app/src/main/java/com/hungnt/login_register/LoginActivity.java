14d58f8de5bdac3ebf9033c282d81eaacf655a4b
HungNT <ilu199x@gmail.com>
2015-07-04 17:46:49 +0700

    Insert some entry in Register

--- app/src/main/java/com/hungnt/login_register/LoginActivity.java
+++ app/src/main/java/com/hungnt/login_register/LoginActivity.java
@@ -3,6 +3,7 @@ package com.hungnt.login_register;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +12,13 @@ import android.widget.TextView;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
+    public static final int GET_REQUEST_CODE = 1;
+    public static final String EXTRA_EMAIL = "com.hungnt.login_register.email";
+
     Button bLogin;
     EditText etUsername,etPassword;
     TextView btRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,25 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.bLogin:
+                break;
             case R.id.tvRegister:
                 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
+                intent.putExtra(EXTRA_EMAIL,etUsername.getText().toString());
+                startActivityForResult(intent, GET_REQUEST_CODE);
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.i("LoginAct","onActivityResult");
+        if(requestCode == GET_REQUEST_CODE)
+            if(resultCode==RESULT_OK) {
+                etUsername.setText(data.getStringExtra("email"));
+                etPassword.setText("");
+            }
     }
 }
--- app/src/main/java/com/hungnt/login_register/RegisterActivity.java
+++ app/src/main/java/com/hungnt/login_register/RegisterActivity.java
@@ -1,31 +1,69 @@
 package com.hungnt.login_register;
 
 import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
-public class RegisterActivity extends Activity implements View.OnClickListener{
+public class RegisterActivity extends Activity implements View.OnClickListener {
 
     Button bRegister;
+    EditText etEmail;
+    EditText etPassword;
+    EditText etRepassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etRepassword = (EditText) findViewById(R.id.etRePassword);
+
         bRegister = (Button) findViewById(R.id.bRegister);
+
+        //Get the data from intent
+        Intent intent = getIntent();
+        String email = intent.getStringExtra(LoginActivity.EXTRA_EMAIL);
+
+        etEmail.setText(email);
 
         bRegister.setOnClickListener(this);
 
     }
 
+    public void showAlert(View view) {
+        Builder builder = new Builder(this);
+        builder.setMessage("Password doesn't match!")
+                .setTitle("Warning")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        builder.show();
+    }
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.bRegister:
-                finish();
+                if (etPassword.getText().toString().equals(""));
+                else if (etPassword.getText().toString().equals(etRepassword.getText().toString())) {
+                    String email = etEmail.getText().toString();
+                    Intent intent = getIntent();
+                    intent.putExtra("email", email);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
         }
     }
 }
